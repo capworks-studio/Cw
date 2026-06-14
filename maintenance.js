c22b6b932731102434c00cc3d67b070d047c8b54
capworks-studio <capkychannel@gmail.com>
2026-06-14 14:00:49 +0200

    Update maintenance.js

--- maintenance.js
+++ maintenance.js
@@ -2,5 +2,5 @@
  * CapWorks Studio — Fichier de maintenance
  */
 
-const IS_MAINTENANCE = true;
+const IS_MAINTENANCE = false;
 const MAINTENANCE_MESSAGE = "Le site est en maintenance. Nous revenons très vite !";
